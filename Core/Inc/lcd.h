99ce2c8fabc2f113cbd9157a0c4da1834d641e2a
Ashton <ashwgage@gmail.com>
2026-09-24 08:45:06 -0700

    Cleaning up lcd header

--- Core/Inc/lcd.h
+++ Core/Inc/lcd.h
@@ -1,20 +1,7 @@
 #ifndef LCD_H
 #define LCD_H
 
-#include "main.h"
 #include <stdint.h>
-
-/*
- * LCD library (HD44780-compatible, 4-bit / nibble mode)
- * -----------------------------------------------------
- * RS  : PB0
- * R/W : PB1
- * E   : PB2
- * DB4 : PB4
- * DB5 : PB5
- * DB6 : PB6
- * DB7 : PB7
- */
 
 void LCD_init(void);
 void LCD_command(uint8_t command);
@@ -23,4 +10,4 @@ void LCD_write_string(const char *text);
 void LCD_set_cursor(uint8_t row, uint8_t column);
 void LCD_clear(void);
 
-#endif /* LCD_H */
+#endif
